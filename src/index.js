715560aea7ed37b91c353f1eed2191069415419f
mrmcscreamypant <161328176+mrmcscreamypant@users.noreply.github.com>
2024-12-09 14:59:43 +0000

    You have no idea how happy I am to see this

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter, Routes, Route} from "react-router";
+import { HashRouter, Routes, Route, useLocation} from "react-router";
+import {PageTransitions} from '@14islands/react-page-transitions'
+
+import "./css/transitions"
 
 import App from "./pages/App";
 import Four0Four from "./pages/404";
@@ -8,12 +11,21 @@ import RouteRegestry from "./componenets/RouteRegestry";
 
 const root = document.getElementById("root");
 
+function Main() {
+  const location = useLocation()
+  return (
+    <PageTransitions pageName={location.pathname}>
+      <Routes location={location}>
+        <Route path="/" element={<App />} />
+        {RouteRegestry()}
+        <Route path="*" element={<Four0Four />} /> {/* points to 404 page, always at the bottom */}
+      </Routes>
+    </PageTransitions>
+  )
+}
+
 ReactDOM.createRoot(root).render(
   <HashRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      {RouteRegestry()}
-      <Route path="*" element={<Four0Four />} /> {/* points to 404 page, always at the bottom */}
-    </Routes>
+    <Main></Main>
   </HashRouter>
 );
