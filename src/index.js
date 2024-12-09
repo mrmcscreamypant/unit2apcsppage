b0bd989517e4e47ab5b4f356bb854b75af74f693
mrmcscreamypant <161328176+mrmcscreamypant@users.noreply.github.com>
2024-12-09 15:03:06 +0000

    Stupid merge

--- src/index.js
+++ src/index.js
@@ -3,27 +3,13 @@ import ReactDOM from "react-dom/client";
 import { HashRouter, Routes, Route, useLocation} from "react-router";
 import {PageTransitions} from '@14islands/react-page-transitions'
 
-import "./css/transitions"
+import "./css/transition.css"
 
 import App from "./pages/App";
 import Four0Four from "./pages/404";
-import AnimationLayout from "./animationLayout"
 import RouteRegestry from "./componenets/RouteRegestry";
 
 const root = document.getElementById("root");
-
-function Main() {
-  const location = useLocation()
-  return (
-    <PageTransitions pageName={location.pathname}>
-      <Routes location={location}>
-        <Route path="/" element={<App />} />
-        {RouteRegestry()}
-        <Route path="*" element={<Four0Four />} /> {/* points to 404 page, always at the bottom */}
-      </Routes>
-    </PageTransitions>
-  )
-}
 
 function Main() {
   const location = useLocation()
